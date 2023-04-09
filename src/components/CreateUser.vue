f24b8328c862a64a94a0238c0f47a7e4c7f7431c
Vlad <hrinyov@gmail.com>
2023-04-10 00:20:43 +0300

    update

---
+++ src/components/CreateUser.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="form-wrapper">
+    <form @submit.prevent="handleSubmit">
+      <label>
+        First name:
+        <input type="text" v-model="firstName" required>
+      </label>
+      <label>
+        Last name:
+        <input type="text" v-model="lastName" required>
+      </label>
+      <label>
+        Email:
+        <input type="email" v-model="email" required>
+      </label>
+      <label>
+        Phone number:
+        <input type="tel" v-model="phoneNumber" required>
+      </label>
+      <button type="submit">Create User</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phoneNumber: '',
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const userData = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        phoneNumber: this.phoneNumber,
+      };
+      console.log(userData); 
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-wrapper {
+  border: 2px solid black;
+  padding: 10px;
+  width: 300px;
+  margin: 0 auto;
+}
+
+form label {
+  display: block;
+  margin-bottom: 6px;
+}
+
+form input {
+  width: 100%;
+  padding: 5px;
+  margin-bottom: 5px;
+  box-sizing: border-box;
+}
+
+button[type="submit"] {
+  display: block;
+  margin: 10px auto;
+
+}
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,27 +1,85 @@
 <template>
+  <div class="main-wrapper">
   <div class="main">
     <button class="add-user" @click="VeiwForm()">Create user</button>
+    <CreateUser v-show="formOpen"/>
   </div>
+  <div class="users-table">
+<table>
+  <thead>
+    <tr>
+      <th>Username</th>
+      <th>Email</th>
+      <th>Phone Number</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="user in users" :key="user.id">
+      <td>{{ user.username }}</td>
+      <td>{{ user.email }}</td>
+      <td>{{ user.phonenumber }}</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+  </div>
+
 </template>
 <script >
+
+import axios from 'axios';
+import CreateUser from '../components/CreateUser.vue';
 export default {
+  data() {
+    return {
+      formOpen: false,
+      users: []
+    }
+  },
+  components: {
+  CreateUser
+  },
+  mounted() {
+  axios.get('http://localhost:8080/users')
+  .then(response => {
+    this.users = response.data;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+  },
   methods: {
-    VeiwForm(){
-      console.log('form');
+    VeiwForm() {
+    this.formOpen = !this.formOpen;
     }
   }
 }
+{
+
+}
 </script>
 <style>
-/* .add-user {
+.add-user {
+  margin: 20px;
+  font-size: 20px;
+}
+.main-wrapper {
+  text-align: center;
+}
+.main {
+  display: block;
+}
+.users-table table {
+  border: 2px solid black;
+
+}
+.users-table td {
+  border: 1px solid black;
+}
+.users-table {
+  display: flex;
+  justify-content: center;
+  font-size: 20px;
   margin-top: 20px;
-  background-color:rgb(119, 210, 103);
-  cursor: pointer;
 }
-.add-user:hover {
-  border: 3px solid black;
-}
-.add-user:active {
-  background-color: rgb(93, 164, 80);
-} */
 </style>
