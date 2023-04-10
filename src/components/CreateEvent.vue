4ac4101a5b73a5514e20e0eb1034d3dfa461d58f
Vlad <hrinyov@gmail.com>
2023-04-11 00:41:41 +0300

    start_config_forms

---
+++ src/components/CreateEvent.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="form-wrapper">
+  <form @submit.prevent="createEvent">
+    <label for="title">Title:</label>
+    <input type="text" id="title" v-model="title" required>
+    <br>
+    <label for="description">Description:</label>
+    <textarea id="description" v-model="description" required></textarea>
+    <br>
+    <label for="startDate">Start Date:</label>
+    <input type="datetime-local" id="startDate" v-model="startDate" required>
+    <br>
+    <label for="endDate">End Date: </label>
+    <input type="datetime-local" id="endDate" v-model="endDate" required>
+    <br>
+    <button type="submit">Create Event</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      description: '',
+      startDate: '',
+      endDate: '',
+    }
+  },
+  props: ["userID"],
+  methods: {
+    createEvent() {
+      const newEvent = {
+        title: this.title,
+        description: this.description,
+        startDate: this.startDate,
+        endDate: this.endDate,
+        userId: this.userID
+      };
+      console.log(newEvent);
+      this.clearForm();
+
+      // Add code here to send the new event to your back-end API
+    },
+    clearForm(){
+      this.title = '';
+      this.description = '';
+      this.startDate = '';
+      this.endDate = '';
+    }
+
+  }
+}
+
+</script>
+
+<style scoped>
+.form-wrapper {
+  border: 2px solid black;
+  padding: 10px;
+  width: 300px;
+  margin: 0 auto;
+}
+
+form label {
+  display: block;
+  margin-bottom: 6px;
+}
+
+form input {
+  width: 100%;
+  padding: 5px;
+  margin-bottom: 5px;
+  box-sizing: border-box;
+}
+
+button[type="submit"] {
+  display: block;
+  margin: 10px auto;
+
+}
+
+</style>
